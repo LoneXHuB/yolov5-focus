39f5923c179748d47fe130e40a97607256a7ae1d
LoneX <elouanes.khelifi@uqtr.ca>
2022-08-03 15:47:29 -0400

    cancel that

--- train.py
+++ train.py
@@ -614,8 +614,8 @@ def main(opt, callbacks=Callbacks()):
             'perspective': (0, 0.0, 0.001),  # image perspective (+/- fraction), range 0-0.001
             'flipud': (1, 0.0, 1.0),  # image flip up-down (probability)
             'fliplr': (0, 0.0, 1.0),  # image flip left-right (probability)
-            'mosaic': (0, 0.0, .0),  # image mixup (probability)
-            'mixup': (0, 0.0, 0.0),  # image mixup (probability)
+            'mosaic': (1, 0.0, 1.0),  # image mixup (probability)
+            'mixup': (1, 0.0, 1.0),  # image mixup (probability)
             'copy_paste': (1, 0.0, 1.0)}  # segment copy-paste (probability)
 
         with open(opt.hyp, errors='ignore') as f:
