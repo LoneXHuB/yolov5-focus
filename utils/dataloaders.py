6178144fcb4a891b553119bac59b2a4eb1a14064
LoneX <elouanes.khelifi@uqtr.ca>
2022-08-05 17:36:51 -0400

    no infinite loader

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -130,7 +130,7 @@ def create_dataloader(path,
     nw = min([os.cpu_count() // max(nd, 1), batch_size if batch_size > 1 else 0, workers])  # number of workers
     print(f"{nw} number of workers --------")
     sampler = None if rank == -1 else distributed.DistributedSampler(dataset, shuffle=shuffle)
-    loader = DataLoader if image_weights else InfiniteDataLoader  # only DataLoader allows for attribute updates
+    loader = DataLoader #if image_weights else InfiniteDataLoader  # only DataLoader allows for attribute updates
     return loader(dataset,
                   batch_size=None,
                   shuffle=shuffle and sampler is None,
