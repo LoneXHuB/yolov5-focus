2d691bc4743a253a8d3edba3e9454838b4398874
LoneX <elouanes.khelifi@uqtr.ca>
2022-08-08 14:55:42 -0400

    sent model in

--- utils/loss.py
+++ utils/loss.py
@@ -163,7 +163,7 @@ class ComputeLoss:
                 print(pbox.size())
                 print(pcls.size())
                 resnt_classifier = ResNet50(3, self.nc)
-                resnetplcls = apply_classifier_lx(pbox,pcls,im[b],im0s[b])
+                resnetplcls = apply_classifier_lx(pbox, pcls, resnt_classifier, im[b], im0s[b])
 
                 """if(infer is not None):
                     gn = torch.tensor(img.shape)[[1, 0, 1, 0]] 
